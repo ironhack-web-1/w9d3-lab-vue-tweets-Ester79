2bfcfd9cdd84323a7813c0f9ab5b7402d2401a94
Ester79 <esterramoscuervo@hotmail.com>
2022-09-05 22:20:48 +0200

    Iteration 8

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -10,16 +10,7 @@
         <Timestamp :time="timestamp"></Timestamp>
       </div>
       <Message :message="message"></Message>
-
-     
-
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <Actions></Actions>
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
@@ -31,6 +22,7 @@ import ProfileImage from "./ProfileImage.vue";
 import User from "./User.vue";
 import Timestamp from "./Timestamp.vue";
 import Message from "./Message.vue";
+import Actions from "./Actions.vue";
 export default {
   name: 'Tweet',
   props: {
@@ -42,7 +34,7 @@ export default {
     message: String,
     image: String,
   },
-  components: { ProfileImage, User, Timestamp, Message }
+  components: { ProfileImage, User, Timestamp, Message, Actions }
 }
 </script>
 
